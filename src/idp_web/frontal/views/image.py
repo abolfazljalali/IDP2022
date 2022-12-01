a85ca3b0a0cb3c68d034ae73f3998bb509812fb0
Kandik <58585761+Kandik@users.noreply.github.com>
2022-12-01 22:28:04 +0200

    Readability refactoring

--- src/idp_web/frontal/views/image.py
+++ src/idp_web/frontal/views/image.py
@@ -28,14 +28,18 @@ def image_list(request):
             Returns:
                     Model (Image): A serialized dict/json of the Image Model.
     '''
+
     try:
-        retrived_image = models.Image.objects.all()
-        response_data = serializers.serialize('json', retrived_image, fields=('pk', 'file_name','file_type', 'bands'))
+        retrieved_image = models.Image.objects.all()
+        response_data = serializers.serialize('json', retrieved_image, fields=('pk', 'file_name','file_type', 'bands'))
+
     except models.Image.DoesNotExist:
-        errorResponse = models.ErrorResponse()
-        errorResponse.error_message = f'No data exists.'
-        response_data = serializers.serialize('json', [errorResponse])
+        error_response = models.ErrorResponse()
+        error_response.error_message = f'No data exists.'
+        response_data = serializers.serialize('json', [error_response])
+
     return HttpResponse(response_data, content_type='application/json')
+
 
 @login_required(login_url='/admin/login/')
 def image_by_id(request, image_id):
@@ -48,14 +52,18 @@ def image_by_id(request, image_id):
             Returns:
                     Model (Image): A serialized dict/json of the Image Model.
     '''
+
     try:
-        retrived_image = models.Image.objects.get(pk=image_id)
-        response_data = serializers.serialize('json', [retrived_image], fields=('pk', 'file_name','file_type', 'bands'))
+        retrieved_image = models.Image.objects.get(pk=image_id)
+        response_data = serializers.serialize('json', [retrieved_image], fields=('pk', 'file_name','file_type', 'bands'))
+
     except models.Image.DoesNotExist:
-        errorResponse = models.ErrorResponse()
-        errorResponse.error_message = f'The image with image_id:{image_id} does not exist.'
-        response_data = serializers.serialize('json', [errorResponse])
+        error_response = models.ErrorResponse()
+        error_response.error_message = f'The image with image_id:{image_id} does not exist.'
+        response_data = serializers.serialize('json', [error_response])
+
     return HttpResponse(response_data, content_type='application/json')
+
 
 @login_required(login_url='/admin/login/')
 def image_by_tag(request, tag_id):
@@ -68,15 +76,19 @@ def image_by_tag(request, tag_id):
             Returns:
                     Model (Image): A serialized dict/json of the Image Model.
     '''
+
     try:
-        retrived_image = models.ImageTag.objects.filter(tag_id=tag_id)
-        retrived_image = [models.Image.objects.get(pk=image_id.id) for image_id in retrived_image]
-        response_data = serializers.serialize('json', retrived_image)
+        retrieved_image = models.ImageTag.objects.filter(tag_id=tag_id)
+        retrieved_image = [models.Image.objects.get(pk=image_id.id) for image_id in retrieved_image]
+        response_data = serializers.serialize('json', retrieved_image)
+
     except models.Image.DoesNotExist:
-        errorResponse = models.ErrorResponse()
-        errorResponse.error_message = f'The images with tag_id:{tag_id} does not exist.'
-        response_data = serializers.serialize('json', [errorResponse], fields=('error_message'))
+        error_response = models.ErrorResponse()
+        error_response.error_message = f'The images with tag_id:{tag_id} does not exist.'
+        response_data = serializers.serialize('json', [error_response], fields=('error_message'))
+
     return HttpResponse(response_data, content_type='application/json')
+
 
 @login_required(login_url='/admin/login/')
 def image_preview(request, image_id, page_id):
@@ -91,27 +103,33 @@ def image_preview(request, image_id, page_id):
             Returns:
                     Model (Image): A serialized dict/json of the Image Model.
     '''
+
     try:
-        retrived_image = models.Image.objects.get(pk=image_id)
-        image_path = pathlib.Path(os.path.join(retrived_image.directory_path, retrived_image.file_name))
+        retrieved_image = models.Image.objects.get(pk=image_id)
+        image_path = pathlib.Path(os.path.join(retrieved_image.directory_path, retrieved_image.file_name))
+
         with TiffFile(image_path) as tif_image:
             raw_img = tif_image.pages[page_id].asarray()
             img = None
+
             if page_id == 0:
                 img = Image.fromarray(raw_img, 'RGB')
             else:
                 img = Image.fromarray(raw_img, 'L')
+
             data = BytesIO()
             img.save(data, "JPEG") # pick your format
             data64 = base64.b64encode(data.getvalue())
             image_uri = u'data:img/jpeg;base64,'+data64.decode('utf-8')
             response_data = {'images':[{'img': image_uri, 'name':image_path.name}]}
+
         return render(request, 'frontal/index.html', response_data)
 
     except models.Image.DoesNotExist:
-        errorResponse = models.ErrorResponse()
-        errorResponse.error_message = f'The image with image_id:{image_id} does not exist.'
-        response_data = serializers.serialize('json', [errorResponse])
+        error_response = models.ErrorResponse()
+        error_response.error_message = f'The image with image_id:{image_id} does not exist.'
+        response_data = serializers.serialize('json', [error_response])
+
     return HttpResponse(response_data, content_type='application/json')
 
 
@@ -143,64 +161,82 @@ def image_insert(request):
         with open(file_full_path, 'wb+') as destination:
             for chunk in file.chunks():
                 destination.write(chunk)
+
     def handle_uploaded_details(tif_img, image):
+
         for index, page in enumerate(tif_img.pages):
             first_page = models.Page()
             first_page.image = image
             first_page.page_number = index
             first_page.save()
+
             for tag in page.tags:
                 tag_instance, _ = models.Tag.objects.get_or_create(name=tag.name)
                 instance_tag = None
+
                 if index == 0:
                     instance_tag = models.ImageTag()
                     instance_tag.image = image
                 else:
                     instance_tag = models.PageTag(page=first_page)
+
                 instance_tag.tag = tag_instance
                 instance_tag.value = tag.value
                 instance_tag.save()
 
-
     if not request.user.is_authenticated:
         print(request.user)
+
         return HttpResponseForbidden()
     
     if request.method == 'POST':
         form = ImageUploadForm(request.POST, request.FILES)
+
         if form.is_valid():
             file_full_path = os.path.join(settings.IMAGE_FILE_DIR, request.POST['file_name'])
             handle_uploaded_file(file_full_path, request.FILES['file'])
+
             try:
                 with TiffFile(file_full_path) as tiff_image:
                     file_format = pathlib.Path(file_full_path).suffix
+
                     new_image, created = models.Image.objects.get_or_create(
                         directory_path  = settings.IMAGE_FILE_DIR,
                         file_name       = request.POST['file_name'],
                         bands           = len(tiff_image.pages),
                         file_type       = models.FileFormat.objects.filter(value=file_format)[0])
+
                     if not created:
                         new_image.delete()
                         new_image = models.Image()
+
                     new_image.directory_path    = settings.IMAGE_FILE_DIR
                     new_image.file_name         = request.POST['file_name']
                     new_image.bands             = len(tiff_image.pages)
                     new_image.file_type         = models.FileFormat.objects.filter(value=file_format)[0]
+
                     new_image.save()
                     handle_uploaded_details(tiff_image, new_image)
                     form.clean()
+
             except Exception as e:
+
                 if os.path.exists(file_full_path):
                     os.remove(file_full_path)
-                    errorResponse = models.ErrorResponse()
-                    errorResponse.error_message = f'Error while storing the information. Uploaded file was removed. Try again! \n{e}'
-                    response_data = serializers.serialize('json', [errorResponse], fields=('error_message'))
+                    error_response = models.ErrorResponse()
+                    error_response.error_message = f'Error while storing the information. Uploaded file was removed. Try again! \n{e}'
+                    response_data = serializers.serialize('json', [error_response], fields=('error_message'))
+
                 return HttpResponse(response_data, content_type='application/json')
+
             return render(request, 'frontal/image/upload.html', {'form': form, 'message':'Image was stored successfully.'})
+
         else:
             print(form.errors.as_data())
+
     else:
         form = ImageUploadForm()
+
     return render(request, 'frontal/image/upload.html', {'form': form})
 
 
@@ -222,6 +258,7 @@ def image_update(request):
             Returns:
                     Model (OperationResult): A serialized dict/json of operation result.
     '''
+
     if not request.user.is_authenticated:
         return HttpResponseForbidden()
 
@@ -229,14 +266,15 @@ def image_update(request):
         put = QueryDict(request.body)
         image_pk = int(put['image_id'])
         current_image = models.Image.objects.get(pk=image_pk)
+
         current_image.directory_path    = put['directory_path']
         current_image.file_name         = put['file_name']
         current_image.file_type         = int(put['file_type'])
         current_image.save()
+
         return HttpResponse('Data updated successfully.')
 
     return HttpResponse(request.method)
-
 
 
 @login_required(login_url='/admin/login/')
@@ -250,7 +288,6 @@ def image_delete(request):
                     Model (OperationResult): A serialized dict/json of operation result.
     '''
 
-    
     if not request.user.is_authenticated:
         return HttpResponseForbidden()
 
@@ -260,8 +297,10 @@ def image_delete(request):
         image_pk = int(delete['image_id'])
         current_image = models.Image.objects.get(pk=image_pk)
         image_path = os.path.join(current_image.directory_path, current_image.file_name)
+
         if os.path.exists(image_path):
             os.remove(image_path)
+
         current_image.delete()
         return HttpResponse('Data deleted successfully.')
 
@@ -280,17 +319,20 @@ def image_download(request, image_id):
     '''
     if not request.user.is_authenticated:
         return HttpResponseForbidden()
+
     print(settings.MEDIA_ROOT)
     current_image = models.Image.objects.get(pk=image_id)
     print(os.path.join(current_image.directory_path, current_image.file_name))
     file_path = os.path.join(settings.IMAGE_FILE_DIR, current_image.file_name)
     print(os.path.exists(file_path))
+
     if os.path.exists(file_path):
         print(file_path)
+
         with open(file_path, 'rb') as fh:
             response = HttpResponse(fh.read(), content_type="image/tiff")
             response['Content-Disposition'] = 'inline; filename=' + current_image.file_name.replace(' ', '_').replace(',', '-')
+
             return response
+
     raise Http404
-
-
