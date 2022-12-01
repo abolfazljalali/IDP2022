eedcc2ff61796da8340960375ed3e42a86a0a1fe
Kandik <58585761+Kandik@users.noreply.github.com>
2022-12-01 22:29:21 +0200

    Readability refactoring

--- src/idp_web/frontal/views/mask.py
+++ src/idp_web/frontal/views/mask.py
@@ -25,13 +25,16 @@ def mask_list(request):
             Returns:
                     Model (Mask): A serialized dict/json of the Mask Model.
     '''
+    
     try:
-        retrived_mask = models.Mask.objects.all()
-        response_data = serializers.serialize('json', retrived_mask)
+        retrieved_mask = models.Mask.objects.all()
+        response_data = serializers.serialize('json', retrieved_mask)
+        
     except models.Mask.DoesNotExist:
-        errorResponse = models.ErrorResponse()
-        errorResponse.error_message = f'No data exists.'
-        response_data = serializers.serialize('json', [errorResponse])
+        error_response = models.ErrorResponse()
+        error_response.error_message = f'No data exists.'
+        response_data = serializers.serialize('json', [error_response])
+        
     return HttpResponse(response_data, content_type='application/json')
 
 
@@ -45,14 +48,18 @@ def mask_by_id(request, mask_id):
             Returns:
                     Model (Mask): A serialized dict/json of the Mask Model.
     '''
+    
     try:
-        retrived_mask = models.Mask.objects.get(pk=mask_id)
-        response_data = serializers.serialize('json', [retrived_mask])
+        retrieved_mask = models.Mask.objects.get(pk=mask_id)
+        response_data = serializers.serialize('json', [retrieved_mask])
+        
     except models.Mask.DoesNotExist:
-        errorResponse = models.ErrorResponse()
-        errorResponse.error_message = f'The mask with mask_id:{mask_id} does not exist.'
-        response_data = serializers.serialize('json', [errorResponse])
+        error_response = models.ErrorResponse()
+        error_response.error_message = f'The mask with mask_id:{mask_id} does not exist.'
+        response_data = serializers.serialize('json', [error_response])
+        
     return HttpResponse(response_data, content_type='application/json')
+
 
 def mask_by_image_id(request, image_id):
     '''
@@ -64,14 +71,17 @@ def mask_by_image_id(request, image_id):
             Returns:
                     Model (Mask): A serialized dict/json of the Mask Model.
     '''
+
     try:
-        retrived_mask = models.ImageMask.objects.filter(image_id=image_id)
-        retrived_mask = [models.Mask.objects.get(pk=mask_id.id) for mask_id in retrived_mask]
-        response_data = serializers.serialize('json', retrived_mask)
+        retrieved_mask = models.ImageMask.objects.filter(image_id=image_id)
+        retrieved_mask = [models.Mask.objects.get(pk=mask_id.id) for mask_id in retrieved_mask]
+        response_data = serializers.serialize('json', retrieved_mask)
+
     except models.Image.DoesNotExist:
-        errorResponse = models.ErrorResponse()
-        errorResponse.error_message = f'The masks with image_id:{image_id} does not exist.'
-        response_data = serializers.serialize('json', [errorResponse])
+        error_response = models.ErrorResponse()
+        error_response.error_message = f'The masks with image_id:{image_id} does not exist.'
+        response_data = serializers.serialize('json', [error_response])
+
     return HttpResponse(response_data, content_type='application/json')
 
 
@@ -91,29 +101,39 @@ def mask_insert(request):
             Returns:
                     Model (OperationResult): A serialized dict/json of operation result.
     '''
+
     def handle_uploaded_file(file_full_path, file):
         with open(file_full_path, 'wb+') as destination:
             print('file is writing')
+
             for chunk in file.chunks():
                 destination.write(chunk)
 
     if not request.user.is_authenticated:
         print(request.user)
+
         return HttpResponseForbidden()
     
     if request.method == 'POST':
         form = MaskUploadForm(request.POST, request.FILES)
+
         if form.is_valid():
             file_full_path = os.path.join(settings.MEDIA_ROOT, request.POST['file_name'])
             handle_uploaded_file(file_full_path, request.FILES['file'])
             new_image = models.Image()
+
             new_image.directory_path    = settings.MEDIA_ROOT
             new_image.file_name         = request.POST['file_name']
             new_image.file_type         = int(request.POST['file_type'])
+
             new_image.save()
+
             return HttpResponse('NICE!')
+
         else:
             print(form.errors.as_data())
+
     else:
         form = MaskUploadForm()
+
     return render(request, 'frontal/image/upload.html', {'form': form})
